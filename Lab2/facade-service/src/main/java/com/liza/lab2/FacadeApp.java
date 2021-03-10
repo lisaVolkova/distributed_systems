6811999ed066f9ced76f9346d4a587183e6e3d81
elanvo <elanvo@ukr.net>
2021-03-10 19:27:54 +0200

    Lab2
    Add lab2 project to git

---
+++ Lab2/facade-service/src/main/java/com/liza/lab2/FacadeApp.java
@@ -0,0 +1,13 @@
+package com.liza.lab2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FacadeApp {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FacadeApp.class, args);
+    }
+
+}
---
+++ Lab2/logging-service/src/test/java/com/liza/logging/demo/LoggingServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.liza.logging.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LoggingServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
