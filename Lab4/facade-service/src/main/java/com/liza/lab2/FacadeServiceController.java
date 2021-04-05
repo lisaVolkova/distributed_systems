509ff99cda49937b877db3c34577884cd20a4079
elanvo <elanvo@ukr.net>
2021-04-05 21:44:09 +0300

    Lab4
    Lab4 impl

--- Lab4/facade-service/src/main/java/com/liza/lab2/FacadeServiceController.java
+++ Lab4/facade-service/src/main/java/com/liza/lab2/FacadeServiceController.java
@@ -8,26 +8,37 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.reactive.function.client.WebClient;
 import reactor.core.publisher.Mono;
 
+import java.util.Random;
 import java.util.UUID;
+import java.util.function.Consumer;
+import java.util.function.Function;
+import java.util.function.Predicate;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
 @RestController
 public class FacadeServiceController {
-    private final WebClient logging = WebClient.create("http://localhost:8088/");
-    private final WebClient messaging = WebClient.create("http://localhost:8089/");
+
+    private final WebClient messaging = WebClient.create("http://localhost:8081/");
+    private final WebClient[] loggingClients = {
+            WebClient.create("http://localhost:8087/"),
+            WebClient.create("http://localhost:8088/"),
+            WebClient.create("http://localhost:8089/")
+    };
     private Logger logger = Logger.getGlobal();
 
     @PostMapping("/facade")
     public Mono<Void> facade(@RequestBody String text) {
         Message message = new Message(UUID.randomUUID(), text);
         logger.log(Level.ALL, text);
-        return logging.post()
+        Mono<Void> voidd =  getRandomLoggingClient()
+                .post()
                 .uri("/log")
                 .contentType(MediaType.APPLICATION_JSON)
                 .body(Mono.just(message), Message.class)
                 .retrieve()
                 .bodyToMono(Void.class);
+        return voidd;
     }
 
     @GetMapping("/facade")
@@ -37,11 +48,20 @@ public class FacadeServiceController {
                                     .retrieve()
                                     .bodyToMono(String.class);
 
-        Mono<String> logs = logging.get()
-                                    .uri("/log")
-                                    .retrieve()
-                                    .bodyToMono(String.class);
+        Mono<String> logs = getLogs();
 
         return messages.zipWith(logs, (s, s2) -> s + ": " + s2).onErrorReturn("Error");
+    }
+
+    private WebClient getRandomLoggingClient() {
+        return loggingClients[new Random().nextInt(loggingClients.length)];
+    }
+
+    private Mono<String> getLogs() {
+        return getRandomLoggingClient().get()
+                .uri("/log")
+                .retrieve()
+                .bodyToMono(String.class)
+                .onErrorResume(err -> getLogs());
     }
 }
--- Lab4/logging-service/src/main/java/com/liza/logging/demo/LoggingServiceController.java
+++ Lab4/logging-service/src/main/java/com/liza/logging/demo/LoggingServiceController.java
@@ -1,12 +1,14 @@
 package com.liza.logging.demo;
 
+import com.hazelcast.core.Hazelcast;
+import com.hazelcast.core.HazelcastInstance;
+import com.hazelcast.map.IMap;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.Hashtable;
 import java.util.Map;
 import java.util.UUID;
 import java.util.logging.Level;
@@ -15,17 +17,25 @@ import java.util.logging.Logger;
 @RestController
 public class LoggingServiceController {
     private Logger logger = Logger.getGlobal();
-    private Map<UUID, String> map = new Hashtable<>();
+    private final HazelcastInstance instance = Hazelcast.newHazelcastInstance();
 
     @GetMapping("/log")
     public String getLogs() {
-        logger.log(Level.ALL, map.toString());
-        return map.toString();
+        IMap<UUID, String> map = instance.getMap("map");
+        StringBuilder builder = new StringBuilder();
+        for (Map.Entry entry : map.entrySet()) {
+            builder.append("{" + entry.getKey() + " " + entry.getValue() +"}");
+        }
+        logger.log(Level.ALL, builder.toString());
+        return builder.toString();
     }
 
     @PostMapping("/log")
     public ResponseEntity<Void> log(@RequestBody Message msg) {
+
+        IMap<UUID, String> map = instance.getMap("map");
         logger.log(Level.ALL, msg.toString());
+        System.out.println(msg.getUUID() + " " + msg.getText());
         map.put(msg.getUUID(), msg.getText());
         return ResponseEntity.ok().build();
     }
